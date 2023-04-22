80eefb76591bd1057efbbd5fa72591a502011a71
shaojiang.wang <shaojiang.wang@datayes.com>
2023-04-22 09:57:15 +0800

    feat: 添加useTabs hook

---
+++ hooks/useTabs.ts
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react"
+
+export const useTabs = () => {
+  const [allTabs, setAllTabs] = useState<chrome.tabs.Tab[]>([])
+  useEffect(() => {
+    const updateAllTabs = () => {
+        chrome.tabs.query({}, (tabs) => {
+          setAllTabs(tabs)
+        })
+      }
+    chrome.tabs.query({}, (tabs) => {
+      setAllTabs(tabs)
+    })
+    chrome.tabs.onUpdated.addListener(updateAllTabs)
+    chrome.tabs.onCreated.addListener(updateAllTabs)
+    chrome.tabs.onRemoved.addListener(updateAllTabs)
+    chrome.tabs.onMoved.addListener(updateAllTabs)
+    chrome.tabs.onActivated.addListener(updateAllTabs)
+    chrome.tabs.onDetached.addListener(updateAllTabs)
+    chrome.tabs.onAttached.addListener(updateAllTabs)
+    return () => {
+      chrome.tabs.onUpdated.removeListener(updateAllTabs);
+      chrome.tabs.onCreated.removeListener(updateAllTabs);
+      chrome.tabs.onRemoved.removeListener(updateAllTabs);
+      chrome.tabs.onMoved.removeListener(updateAllTabs);
+      chrome.tabs.onActivated.removeListener(updateAllTabs);
+      chrome.tabs.onDetached.removeListener(updateAllTabs);
+      chrome.tabs.onAttached.removeListener(updateAllTabs);
+    }
+  }, [])
+
+  useEffect(() => {
+    const removeNewTab = () => {
+      chrome.tabs.query({ url: "chrome://newtab/" }).then((allTabs) => {
+        chrome.tabs.remove(allTabs.map((tab) => tab.id))
+      })
+    }
+    chrome.tabs.onCreated.addListener(removeNewTab)
+    return () => {
+      chrome.tabs.onCreated.removeListener(removeNewTab)
+    }
+  }, [])
+
+  return allTabs
+}
